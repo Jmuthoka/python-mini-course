2ec8984484564cb06ce8a9cb2e34f15e786680e0
John_Muthoka <jmuthoka@me.bergen.edu>
2023-05-30 17:14:54 -0400

    completed day 1

--- day 1/helloWorld.py
+++ day 1/helloWorld.py
@@ -1,5 +1,10 @@
 # Output & Input: print, input
-
+""
 # print Hello World!
-
+print ("hello world!")
 # print Hello {user}! using input
+name = input(" enter your name:")
+print("Hello"+ name)
+num = 1 
+ if num == 1:
+  print ("it's 1!")
